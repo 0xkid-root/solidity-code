f8191a8f5912ae0c0feff057ca6404541a9aff22
0xkid-root <prohackers698@gmail.com>
2025-12-09 00:39:52 +0800

    Ether wallet Project on solidity EVM chain

--- EtherWallet.sol
+++ EtherWallet.sol
@@ -1,2 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
+
+contract EtherWallet{
+
+    address payable public owner;
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable{} // koi v banda direct humare contract par amount send kar sakta hai 
+
+modifier onlyModifier(){
+    require(owner == msg.sender,"owner is not correct");
+    _;
+}
+    
+
+    function withdraw() external  onlyModifier{
+
+        payable(owner).transfer(address(this).balance);// yaha hum contract address se hum balance ko owner ke address par transfer kar rh hai 
+
+    }
+
+    function contractbalanceFetch() view public returns(uint){
+        return address(this).balance;
+    }
+
+
+
+
+
+
+
+
+
+}
