57b9c45f20e78b005c3030fba2916c9ed19fd1ec
0xkid-root <prohackers698@gmail.com>
2025-12-10 10:41:59 +0800

    update erc20

--- erc20.sol
+++ erc20.sol
@@ -4,15 +4,22 @@ pragma solidity 0.8.30;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
-// allowance function check karta hai ke owner ne kitne spend krne ka limit diya hai 
+// allowance function checks how much the owner has approved for spending
 
-// transfer function direct transfer kar deta hai 
-// transferform check karta. hai phale allowance hai ke nhi 
+// transfer function directly transfers tokens
+
+// transferFrom checks if allowance exists before transferring
+
+/* 
+If I want to transfer funds to a contract, it can be done with transfer, 
+but the best practice is to use approve then transferFrom. 
+When I approve an address to use my tokens on behalf of me,
+the delegated address needs to call transferFrom to move the tokens.
+*/
 
 
 contract MyToken is ERC20{
-    constructor() ERC20("MYTOKWN","MKT"){
+    constructor() ERC20("MYTOKEN","MKT"){
         _mint(msg.sender,1e25);
     }
-
 }
