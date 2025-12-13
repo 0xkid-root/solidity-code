6bd28e7e3b861a16edd05b201c0a7a466482dffd
0xkid-root <prohackers698@gmail.com>
2025-12-13 11:48:32 +0800

    erc1155

---
+++ erc1155FullyContract.sol
@@ -0,0 +1,7 @@
+ERC1155--->(address to, uint256 id, uint256 amount);
+ERC20 ---->(address to,uint256 amount);
+ERC721 ---->(address to,uint256 tokenId);
+
+
+
+ERC1155--->(address to, uint256 id, uint256 amount);
