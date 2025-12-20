5c03592d5b8615ac27813da3973e3c9d8a134344
0xkid-root <prohackers698@gmail.com>
2025-12-20 10:57:12 +0800

    croudfunding

--- croudingfunding.sol
+++ croudingfunding.sol
@@ -11,10 +11,21 @@ contract CroudFunding{
     uint public deadline;
     uint public minimumContibution;
 
+    struct Request {
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address=>bool) voters;
+    }
+    mapping(uint=>Request) public requests;
+    uint public numRequest;
+
     constructor(uint _target,uint _deadline){
         target=_target;
         manager=msg.sender;
-        minimumContibution=100 ether;
+        minimumContibution=1 ether;
         deadline=block.timestamp + _deadline; // block.timestamp provided current block timestamp
     }
 
@@ -33,11 +44,44 @@ contract CroudFunding{
 
     }
 
-     // contract balance cehck function ----- 
+    // contract balance cehck function ----- 
 
     function getContractBalance() public view returns(uint){
         return address(this).balance;
         
+    }
+
+    function refund() public{
+        require(block.timestamp>deadline && raisedAmount <target, "you are not eliglibility to transfer your fund");
+        require(contributors[msg.sender]>0);
+        address payable user =payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender]=0;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == manager,"only manager can call this function");
+        _;
+    }
+
+    function createRequest(string memory _description,address payable _recipient,uint _value) public onlyOwner{
+        Request storage newRequest = requests[numRequest];
+        numRequest++;
+        newRequest.description=_description;
+        newRequest.recipient=_recipient;
+        newRequest.value=_value;
+        newRequest.completed=false;
+        newRequest.noOfVoters=0;
+
+
+    }
+
+    function voteRequest(uint _requestNo) public{
+        require(contributors[msg.sender]>0,"you must be contributors!");
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.voters[msg.sender]==false,"you have already vote");
+        thisRequest.voters[msg.sender]=true;
+        thisRequest.noOfVoters++;
     }
 
 
